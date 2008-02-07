6f2062fc6b509b49382ab358e8e9d8e24fd7c885
cjcollier <cjcollier@3355ff64-970d-0410-bbe8-d0fbd18be4fb>
2008-02-07 16:31:29 +0000

    2008-02-07T08:27	C.J. Adams-Collier <cjac@colliertech.org> 	* AUTHORS 	- consolidated names and contact info found laying around the 	  source 	* src/Core/Kernel.cs 	- updated copyright date 	- re-formatted license for 80-column editor 	- updated log banner to indicate new date, new authors 	* src/Core/Targets/AutotoolsTarget.cs 	- clarified reasoning behind use of constants in 	  AutotoolsTarget.ParsePCFile 	- reduced length of some long lines using newline/indent 	- added log messages for parsing .pc files, emitting solutions, 	  projects 	- robustified the inter-package dependency resolution target 	- log warning when we can't find assembly for <Reference /> 	- clarified code for case of inability to find embedded 	  autotools.xml 	* src/data/autotools.xml 	- adding system lookup of resgen2 to configure.ac 	- fixed .resource -> .resources typo 	- added a rule to create <foo>.response file containing all 	sources 	- using @<foo>.response on $(CSC) command line instead of listing 	  all source files 	* src/Properties/AssemblyInfo.cs 	- re-formatted license for an 80-column editor 	- added more authors to the AssemblyCopyright attribute 	- bumped version to 2.0.3 	* prebuild.xml 	- bumped version to 2.0.3 	* scripts/autotools.sh 	- if 'mono' is in the path, run Prebuild.exe with it 	- using dirname to capture correct path to prebuild.xml &c

--- Prebuild/src/Core/Kernel.cs
+++ Prebuild/src/Core/Kernel.cs
@@ -1,29 +1,38 @@
 #region BSD License
 /*
-Copyright (c) 2004-2007
+Copyright (c) 2004-2008
 Matthew Holmes (matthew@wildfiregames.com),
 Dan Moorehead (dan05a@gmail.com),
 Rob Loach (http://www.robloach.net),
 C.J. Adams-Collier (cjac@colliertech.org)
 
-Redistribution and use in source and binary forms, with or without modification, are permitted
-provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
-* Redistributions of source code must retain the above copyright notice, this list of conditions 
-  and the following disclaimer. 
-* Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
-  and the following disclaimer in the documentation and/or other materials provided with the 
-  distribution. 
-* The name of the author may not be used to endorse or promote products derived from this software 
-  without specific prior written permission. 
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
 
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+* The name of the author may not be used to endorse or promote
+  products derived from this software without specific prior written
+  permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
 */
 #endregion
 
@@ -322,10 +331,16 @@ namespace Prebuild.Core
 
 		private void LogBanner()
 		{
-			m_Log.Write("Prebuild v" + this.Version);
-			m_Log.Write("Copyright (c) Matthew Holmes, Dan Moorehead and David Hudson");
-			m_Log.Write("See 'prebuild /usage' for help");
-			m_Log.Write();
+                  m_Log.Write("Prebuild v" + this.Version);
+                  m_Log.Write("Copyright (c) 2004-2008");
+                  m_Log.Write("Matthew Holmes (matthew@wildfiregames.com),");
+                  m_Log.Write("Dan Moorehead (dan05a@gmail.com),");
+                  m_Log.Write("David Hudson (jendave@yahoo.com),");
+                  m_Log.Write("Rob Loach (http://www.robloach.net),");
+                  m_Log.Write("C.J. Adams-Collier (cjac@colliertech.org),");
+
+                  m_Log.Write("See 'prebuild /usage' for help");
+                  m_Log.Write();
 		}
 
 		private void ProcessFile(string file)
