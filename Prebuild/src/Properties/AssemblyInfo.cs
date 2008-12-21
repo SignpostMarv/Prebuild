c205651468b6c061a637768bb2e89cbd0071faf8
dmoonfire <dmoonfire@3355ff64-970d-0410-bbe8-d0fbd18be4fb>
2008-12-21 19:18:19 +0000

    Bumped the version for the next version to 2.0.4.

--- Prebuild/src/Properties/AssemblyInfo.cs
+++ Prebuild/src/Properties/AssemblyInfo.cs
@@ -79,7 +79,7 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: AssemblyVersion("2.0.3.*")]
+[assembly: AssemblyVersion("2.0.4.*")]
 
 //
 // Version information for an assembly consists of the following four values:
